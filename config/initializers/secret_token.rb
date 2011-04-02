cfdccf3ccf209f8cea26680d27a46c2d5d56e320
Thu <thu2004@yahoo.se>
2011-04-02 15:35:07 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyLe::Application.config.secret_token = '07834b6a152ca64db198c20b09c9bd4286f6ea8a6e5ef74f8f800bf936d118fc2593cd129575b200f8cfa90810792d828a7284edaa5f721ea6d35c5a5f1e930b'
---
+++ vendor/engines/events/app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ActiveRecord::Base
+
+  acts_as_indexed :fields => [:title, :blurb]
+
+  validates :title, :presence => true, :uniqueness => true
+  
+end
